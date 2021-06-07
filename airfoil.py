7a231db83aaf5d5ff295c2b886d5f5755396e227
Jose-Oviedo <73795389+Jose-Oviedo@users.noreply.github.com>
2021-06-07 19:33:18 +0200

    Lectura de datos
    Se ha implementado la lectura de datos y la separación del conjunto inicial en los subconjuntos train y test

--- airfoil.py
+++ airfoil.py
@@ -3,6 +3,14 @@
 """
 Created on Mon Jun  7 19:03:34 2021
 
-@author: angel
+@author: Angel Cabeza y Jose Luis Oviedo 
 """
+import numpy as np
+from sklearn.model_selection import train_test_split
 
+# Lectura de los datos del problema
+data = np.loadtxt( 'datos/airfoil_self_noise.dat' )
+
+# División del conjunto inicial en dos subconjuntos train y test
+# con train el 80% de los datos y test con el 20%.
+train, test = train_test_split(data, test_size = 0.20, random_state = 123)
